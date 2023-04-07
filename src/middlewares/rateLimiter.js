08f31dd7358a079c12704a66c5f027f14d2eef83
Bharat Paliwal <18uec169@lnmiit.ac.in>
2023-04-07 09:39:49 +0530

    security commit

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -3,6 +3,8 @@ import rateLimit from 'express-rate-limit';
 const ApiRateLimiter = rateLimit({
   windowMs : 60 * 1000, //1 minute
   max : 100,
+  standardHeaders: true,
+  legacyHeaders:false,
   message : "Too many attempts"
 }) 
 
